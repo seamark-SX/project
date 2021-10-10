a8a4986d813b33186a5ba4dd9f8c23b95f2ac21a
seamark-SX <92091164+seamark-SX@users.noreply.github.com>
2021-10-10 13:46:16 +1000

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,35 @@
+const data = {
+    "first_name": "Anna",
+    "last_name": "Green",
+    "job_title": "MARKETING DIRECTOR",
+    "photo": "images/anna green.jpg",
+    "phone": "+34-93-123 45 67",
+    "email": "email@gmail.com",
+    "skype": "skype_number",
+    "linkedin": "link_to_linkedin",
+    "address": "Consejo de Ciento, 312, Barcelona",
+    "skills": [
+        "Creative Development",
+        "Branding & Identity",
+        "Project Management",
+        "Contact Negotiation",
+        "Ad Buying & Planning",
+        "Product Positioning",
+        "Visual Merchandising",
+        "Search Engine Ads",
+        "Process Improvement",
+        "Market Strategizing",
+        "Adobe Creative Suite",
+        "Microsoft Office 2016",
+        "Google Web Analytics",
+        "Fluent in Spanish",
+    ],
+    "education": [
+        ["2008-2010", "University of Alabama English Language Institute"],
+        ["2010-2015", "University of Alabama English Language Institute"],
+    ],
+    "achievements": [
+        "Marketer of the Year Academic Scholorship Deans List: 2006-2007, National Merit Scholarship Corporation"
+    ],
+    "profile": "This paragraph will be you first impressions to recruiters. An effective summary is short, concise and focuses on your most important selling points. You should align your summary with the job requirements by highlighting the key strengths and skills that will truly make you stand out as a highly qualified candidate. Transforming responsibilities into quantifiable achievements with numbers communicates your value."
+};
